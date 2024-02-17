7e3a73d115373ada03fdf742b14d57377467d9f0
Valentijn de Jong <v.m.t.dejong-2@umcutrecht.nl>
2024-02-17 22:17:16 +0100

    added tests for one-stage models
    added tests for one-stage models in metapred

--- tests/testthat/test_metapred_6_one_stage.R
+++ tests/testthat/test_metapred_6_one_stage.R
@@ -1,6 +1,6 @@
 context("metapred 6. One-stage.")
 
-set.seed(1234)
+set.seed(7318)
 n <- 100
 y <- c(rep(0, 2 * n), rep(1, 2 * n))
 x <- rnorm(length(y), y, 1)
@@ -15,17 +15,43 @@ test_that("metapred can estimate one-stage fixed effect models", {
   expect_is(mp_fe, "metapred")
 })
 
+test_that("calibration of metapred one-stage fixed effect models is ok", { 
+  skip_on_cran()
+  f <- y ~ x
+  mp_fe <- metapred(d, "k", formula = f, scope = f, family = binomial, estFUN = glm, two.stage = F, 
+                    perfFUN = list("bin.cal.int", "cal.slope"),
+                    genFUN = list("rema"),
+                    gen.of.perf = "factorial")
+  expect_gte(gen(mp_fe, 1), 0)
+  expect_lte(gen(mp_fe, 1), .01)
+
+  expect_gte(gen(mp_fe, 2), 1)
+  expect_lte(gen(mp_fe, 2), 1.1)
+})
 
 
 test_that("metapred can estimate one-stage random effects models", { 
   skip_on_cran()
   
-  f <- y ~ x + (1|k)
+  f_ri <- y ~ x + (1|k)
   
-  mp_ri <- metapred(d, "k", formula = f, scope = f, family = binomial, estFUN = lme4::glmer, two.stage = F)
+  cal_slope_glmer <- function(p, y, family = binomial, ...) {
+    metamisc:::cal.slope(p, y, estFUN = "glm", family = family)
+  }
+  
+  mp_ri <- metapred(d, "k", formula = f_ri, scope = f_ri, family = binomial, estFUN = lme4::glmer, two.stage = F, 
+                    perfFUN = list("bin.cal.int", "cal_slope_glmer"),
+                    genFUN = list("rema"),
+                    gen.of.perf = "factorial")
   expect_is(mp_ri, "metapred")
   
-  f <- y ~ x + (1 + x | k)
-  mp_re <- metapred(d, "k", formula = f, scope = f, family = binomial, estFUN = lme4::glmer, two.stage = F)
+  expect_gte(gen(mp_ri, 1), 0)
+  expect_lte(gen(mp_ri, 1), .01)
+  
+  expect_gte(gen(mp_ri, 2), 1)
+  expect_lte(gen(mp_ri, 2), 1.1)
+  
+  f_re <- y ~ x + (1 + x | k)
+  mp_re <- metapred(d, "k", formula = f_re, scope = f_re, family = binomial, estFUN = lme4::glmer, two.stage = F)
   expect_is(mp_re, "metapred") 
 })
